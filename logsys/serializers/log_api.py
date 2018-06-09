3373801d45a7cea6b61fa9efa57544de293c782a
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2018-06-09 17:17:33 +0800

    bugfix

--- logsys/serializers/log_api.py
+++ logsys/serializers/log_api.py
@@ -4,9 +4,9 @@ from ordersys.models import OrderProtocol, OrderInfo
 
 class ObtainOrderLogSerializer(serializers.Serializer):
     user_sid = serializers.CharField()
-    oid = serializers.PrimaryKeyRelatedField(queryset=OrderInfo.objects, read_only=True)
+    oid = serializers.PrimaryKeyRelatedField(queryset=OrderInfo.objects)
 
 
 class ObtainOrderProtocolLogSerializer(serializers.Serializer):
     user_sid = serializers.CharField()
-    opid = serializers.PrimaryKeyRelatedField(queryset=OrderProtocol.objects, read_only=True)
+    opid = serializers.PrimaryKeyRelatedField(queryset=OrderProtocol.objects)
