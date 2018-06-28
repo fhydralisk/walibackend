c7af84a11b97e65999d124b191a3e54d3d95e98f
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2018-06-28 22:50:21 +0800

    local memory cache for templates

--- pushsys/funcs/utils/template.py
+++ pushsys/funcs/utils/template.py
@@ -1,12 +1,42 @@
 import json
+import logging
 from pushsys.models import PushTemplate
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
-# TODO: Hook the PushTemplate's post_save. update cache.
+
+# TODO: This shall be move into memcached.
+logger = logging.getLogger(__name__)
+cached_state_templates_dict = None
+
+
+def reload_templates():
+    global cached_state_templates_dict
+    state_templates = PushTemplate.objects.filter(in_use=True)
+    cached_state_templates_dict = {}
+    for template in state_templates:
+        if template.push_state_name:
+            former = cached_state_templates_dict.get(template.push_state_name, [])
+            former.append(template)
+            cached_state_templates_dict[template.push_state_name] = former
+
+    logger.info("Push Template reloaded into cache.")
+
+
+# Hook the PushTemplate's post_save. update cache.
+@receiver(signal=[post_save, post_delete], sender=PushTemplate)
+def update_templates(**kwargs):
+    reload_templates()
 
 
 def get_state_change_template(state_name, state_from, state_after):
-    # TODO: Cache Here.
-    state_templates = PushTemplate.objects.filter(in_use=True, push_state_name=state_name)
+    # Try accessing cached dict first
+    # state_templates = PushTemplate.objects.filter(in_use=True, push_state_name=state_name)
+    if cached_state_templates_dict is None:
+        reload_templates()
+
+    state_templates = cached_state_templates_dict[state_name]
+
     for template in state_templates:
         if template.push_ctx is None:
             continue
