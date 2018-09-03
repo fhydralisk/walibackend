bbef6dda3e9fcf25552c4faf67e9af86aa721e9e
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2018-09-03 21:33:12 +0800

    1及品类获取回归。

--- demandsys/views/catalog.py
+++ demandsys/views/catalog.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from base.views import WLAPIView
 
-from demandsys.funcs.catalog import get_l1_2_3, get_water_content
+from demandsys.funcs.catalog import get_l1_2_3, get_water_content, get_l1
 from demandsys.serializers.catalog import ProductTypeSerializer, ProductWaterContentSerializer, ProductTypeFDictSerializer
 
 
@@ -24,4 +24,13 @@ class GetFTreedProductTypeView(WLAPIView, APIView):
         return self.generate_response(data={
             "product_types": ProductTypeFDictSerializer(l1_types, many=True).data,
             "product_watercontents": ProductWaterContentSerializer(wc, many=True).data,
+        }, context=context)
+
+
+class GetProductTypeL1(WLAPIView, APIView):
+    def get(self, request):
+        data, context = self.get_request_obj(request)
+        l1_types = get_l1()
+        return self.generate_response(data={
+            "product_types": ProductTypeSerializer(l1_types, many=True, nested=False).data,
         }, context=context)
