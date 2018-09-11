3d841fd08b10ca727d17f24754fe6dd1d60afe2b
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2018-09-11 23:03:24 +0800

    CancelReason admin

--- simplified_invite/admin.py
+++ simplified_invite/admin.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import InviteInfo
+from .models import InviteInfo, InviteCancelReason
 
 # Register your models here.
 
-admin.site.register([InviteInfo, ])
+admin.site.register([InviteInfo, InviteCancelReason])
